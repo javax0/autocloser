a370ad2475d62526e3934a99c5dd7d0733abde7e
Peter Verhas <peter@verhas.com>
2019-05-14 22:01:09 +0200

    JavaDoc to AutoCloser

--- src/main/java/javax0/AutoCloser.java
+++ src/main/java/javax0/AutoCloser.java
@@ -3,6 +3,31 @@ package javax0;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+/**
+ * A simle class that helps to handle the automatic closing of resources (objects) that do not implement the
+ * {@link AutoCloseable} interface in a try-with-resources block.
+ * <p>
+ * As a try-with-resources block can only handle a resource that implements the {@link AutoCloseable} interface we have
+ * to envelope the non implementing resource into one that does implement. The envelope is an {@code AutoCloser}
+ * object (more precisely an {@code AutoCloser<T>.Closer} instance) that implements the {@link AutoCloseable} interface
+ * as well as the {@link Supplier<T>} interface. The implemented {@link AutoCloseable#close()} method invokes a
+ * {@link Consumer<Supplier<T>>} that gets the original {@code T} resource. The resource itself can be defined as the
+ * argument to the {@link #useResource(Object)} method. The consumer has to be defined as the argument to the
+ * {@link #closeWith(Consumer)} method.
+ * <p>
+ * The use of the class is
+ * <pre>{@code
+ * try (final var s = AutoCloser.useResource(new NotAutoclosable())
+ *            .closeWith(sp -> sp.get().dispose())) {
+ *        ...
+ *    }
+ * }</pre>
+ * <p>
+ * The closing functionality can also be defined as a {@link Runnable} with an overloadedversion of
+ * {@link #closeWith(Runnable)}.
+ *
+ * @param <T> the type of the original resource.
+ */
 public class AutoCloser<T> {
 
     private final T resource;
@@ -15,12 +40,12 @@ public class AutoCloser<T> {
         return new AutoCloser<>(resource);
     }
 
-    public AutoClosableSupplier closeWith(Consumer<Supplier<T>> closer){
+    public AutoClosableSupplier closeWith(Consumer<Supplier<T>> closer) {
         return new AutoClosableSupplier(closer);
     }
 
-    public AutoClosableSupplier closeWith(Runnable closer){
-        return new AutoClosableSupplier( t -> closer.run());
+    public AutoClosableSupplier closeWith(Runnable closer) {
+        return new AutoClosableSupplier(t -> closer.run());
     }
 
     public class AutoClosableSupplier implements Supplier<T>, AutoCloseable {
